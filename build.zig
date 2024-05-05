aa94ba8ac0f43cc42b740d28cbfd26b517a159ac
tazend <toni.harzendorf@gmail.com>
2024-05-05 16:45:35 +0200

    provide public function to setup include/library dir for slurm

--- build.zig
+++ build.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 const LazyPath = std.Build.LazyPath;
 const path = std.fs.path;
+const Compile = std.Build.Step.Compile;
+const Allocator = std.mem.Allocator;
 
-pub const Options = struct {
-    slurm_dir: ?[]const u8 = null,
-};
+pub fn setupSlurmPath(target: *Compile, slurm_dir: ?[]const u8, allocator: Allocator) !void {
+    const dir: ?[]const u8 = if (slurm_dir) |d| d else std.posix.getenv("SLURM_INSTALL_DIR");
+
+    if (dir) |d| {
+        const incpath = try path.join(allocator, &[_][]const u8{ d, "include" });
+        const libpath = try path.join(allocator, &[_][]const u8{ d, "lib" });
+        target.addIncludePath(.{ .path = incpath });
+        target.addLibraryPath(.{ .path = libpath });
+    }
+}
 
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
@@ -12,11 +21,9 @@ pub fn build(b: *std.Build) !void {
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
+    const allocator = arena.allocator();
 
-    const defaults = Options{};
-    const options = Options{
-        .slurm_dir = b.option([]const u8, "slurm-dir", "Slurm installation directory") orelse defaults.slurm_dir,
-    };
+    const slurm_dir = b.option([]const u8, "slurm-dir", "Slurm installation directory");
 
     const slurm = b.addModule("slurm", .{
         .root_source_file = .{ .path = "src/root.zig" },
@@ -29,10 +36,8 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    if (options.slurm_dir) |dir| {
-        slurm_lib.addIncludePath(.{ .path = try path.join(arena.allocator(), &[_][]const u8{ dir, "include" }) });
-        slurm_lib.addLibraryPath(.{ .path = try path.join(arena.allocator(), &[_][]const u8{ dir, "lib" }) });
-    }
+    try setupSlurmPath(slurm_lib, slurm_dir, allocator);
+
     slurm_lib.linkLibC();
     slurm_lib.linkSystemLibrary("slurm");
     b.installArtifact(slurm_lib);
@@ -44,10 +49,8 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    if (options.slurm_dir) |dir| {
-        tests.addIncludePath(.{ .path = try path.join(arena.allocator(), &[_][]const u8{ dir, "include" }) });
-        tests.addLibraryPath(.{ .path = try path.join(arena.allocator(), &[_][]const u8{ dir, "lib" }) });
-    }
+
+    try setupSlurmPath(tests, slurm_dir, allocator);
 
     tests.linkLibrary(slurm_lib);
     tests.root_module.addImport("slurm", slurm);
