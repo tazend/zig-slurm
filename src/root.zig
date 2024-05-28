53c66a2d3cca9ff420bd47f067d95d811b184b87
tazend <toni.harzendorf@gmail.com>
2024-05-28 23:14:00 +0200

    add function to simply parsing a raw char* coming from slurm

--- src/root.zig
+++ src/root.zig
@@ -12,6 +12,11 @@ pub const slurm_allocator = SlurmAllocator.slurm_allocator;
 pub const init = c.slurm_init;
 pub const deinit = c.slurm_fini;
 
+pub inline fn parseCStr(s: ?[*:0]u8) ?[]const u8 {
+    if (s == null) return null;
+    return std.mem.span(s);
+}
+
 test "slurm_allocator" {
     try std.heap.testAllocator(slurm_allocator);
 }
