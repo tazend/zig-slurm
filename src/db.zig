7dbac616d245e24cb6e88300847edefd2f0414b4
Toni Harzendorf <toni.harzendorf@gmail.com>
2025-04-17 15:39:57 +0200

    db: add Transaction and TransactionFilter structs

--- src/db.zig
+++ src/db.zig
@@ -99,6 +99,33 @@ pub const JobFilterFlags = packed struct(u32) {
     pub usingnamespace common.BitflagMethods(JobFilterFlags, u32);
 };
 
+pub const TransactionFilter = extern struct {
+    accounts: ?*List(CStr) = null,
+    actions: ?*List(CStr) = null,
+    actors: ?*List(CStr) = null,
+    clusters: ?*List(CStr) = null,
+    __format_list: ?*list_t = null,
+    ids: ?*List(CStr) = null,
+    infos: ?*List(CStr) = null,
+    names: ?*List(CStr) = null,
+    time_end: time_t = 0,
+    time_start: time_t = 0,
+    users: ?*List(CStr) = null,
+    with_assoc_info: u16 = NoValue.u16,
+};
+
+pub const Transaction = extern struct {
+    accounts: ?CStr = null,
+    action: u16 = NoValue.u16, // slurmdb_msg_type
+    actors: ?CStr = null,
+    clusters: ?CStr = null,
+    id: u32 = 0,
+    set_info: ?CStr = null,
+    timestamp: time_t = 0,
+    users: ?CStr = null,
+    where_query: ?CStr = null,
+};
+
 pub const WCKey = extern struct {
     accounting_list: ?*list_t = null,
     cluster: ?CStr = null,
