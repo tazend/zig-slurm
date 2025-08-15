010d216260c4bcff56b6fcc89e37a4588fc4bef1
Toni Harzendorf <toni.harzendorf@gmail.com>
2025-08-15 12:20:27 +0200

    build.zig: update search paths and which library to choose
    Add option use_slurmfull so one can specify to use libslurmfull instead
    of libslurm.
    Users are required to use the --search-prefix option of "zig build" to
    add non-standard library search paths

--- build.zig
+++ build.zig
@@ -1,12 +1,9 @@
 const std = @import("std");
 const Compile = std.Build.Step.Compile;
 
-pub fn setupSlurmPath(b: *std.Build, target: *Compile, slurm_dir: ?[]const u8) !void {
-    const dir: ?[]const u8 = if (slurm_dir) |d| d else std.posix.getenv("SLURM_INSTALL_DIR");
-
-    if (dir) |d| {
-        target.addIncludePath(.{ .cwd_relative = (b.fmt("{s}/include", .{d})) });
-        target.addLibraryPath(.{ .cwd_relative = (b.fmt("{s}/lib64", .{d})) });
+pub fn setupSlurmPath(b: *std.Build, target: *Compile) void {
+    for (b.search_prefixes.items) |o| {
+        target.addLibraryPath(.{ .cwd_relative = o });
     }
 }
 
@@ -14,7 +11,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const slurm_dir = b.option([]const u8, "slurm-dir", "Slurm installation directory");
+    const use_slurmfull = b.option(bool, "use-slurmfull", "Whether to use libslurmfull.so or not.") orelse false;
 
     const slurm = b.addModule("slurm", .{
         .root_source_file = b.path("src/root.zig"),
@@ -25,12 +22,13 @@ pub fn build(b: *std.Build) !void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
-    try setupSlurmPath(b, slurm_lib, slurm_dir);
+    setupSlurmPath(b, slurm_lib);
 
-    slurm_lib.linkLibC();
-    slurm_lib.linkSystemLibrary("slurm");
+    const slurm_lib_name = if (use_slurmfull) "slurmfull" else "slurm";
+    slurm_lib.linkSystemLibrary(slurm_lib_name);
     b.installArtifact(slurm_lib);
 
     const test_step = b.step("test", "Run slurm tests");
@@ -41,8 +39,7 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    try setupSlurmPath(b, tests, slurm_dir);
-
+    setupSlurmPath(b, tests);
     tests.linkLibrary(slurm_lib);
     tests.root_module.addImport("slurm", slurm);
     b.installArtifact(tests);
