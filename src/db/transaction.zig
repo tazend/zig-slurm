43b172633a68f24f39847c5223828066c529ac42
Toni Harzendorf <toni.harzendorf@gmail.com>
2025-09-18 17:32:06 +0200

    db.Transaction + Filter: don't rename fields.
    no point in renaming for now, stay in sync with the C API

--- src/db/transaction.zig
+++ src/db/transaction.zig
@@ -7,9 +7,9 @@ const time_t = std.posix.time_t;
 const NoValue = common.NoValue;
 
 pub const Transaction = extern struct {
-    accounts: ?CStr = null,
+    accts: ?CStr = null,
     action: u16 = NoValue.u16, // slurmdb_msg_type
-    actors: ?CStr = null,
+    actor_name: ?CStr = null,
     clusters: ?CStr = null,
     id: u32 = 0,
     set_info: ?CStr = null,
@@ -18,17 +18,17 @@ pub const Transaction = extern struct {
     where_query: ?CStr = null,
 
     pub const Filter = extern struct {
-        accounts: ?*List(CStr) = null,
-        actions: ?*List(CStr) = null,
-        actors: ?*List(CStr) = null,
-        clusters: ?*List(CStr) = null,
-        __format_list: ?*List(*opaque {}) = null,
-        ids: ?*List(CStr) = null,
-        infos: ?*List(CStr) = null,
-        names: ?*List(CStr) = null,
+        acct_list: ?*List(CStr) = null,
+        action_list: ?*List(CStr) = null,
+        actor_list: ?*List(CStr) = null,
+        cluster_list: ?*List(CStr) = null,
+        format_list: ?*List(CStr) = null,
+        id_list: ?*List(CStr) = null,
+        info_list: ?*List(CStr) = null,
+        name_list: ?*List(CStr) = null,
         time_end: time_t = 0,
         time_start: time_t = 0,
-        users: ?*List(CStr) = null,
+        user_list: ?*List(CStr) = null,
         with_assoc_info: u16 = NoValue.u16,
     };
 };
