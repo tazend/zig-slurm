cf546450a1b84e27f134fb51ed15ca91f812224a
Toni Harzendorf <toni.harzendorf@gmail.com>
2026-03-24 18:34:16 +0100

    db.archive: add PurgeAfter and PurgeFlags

--- src/db/archive.zig
+++ src/db/archive.zig
@@ -12,12 +12,25 @@ pub const Archive = extern struct {
         archive_dir: ?CStr = null,
         archive_script: ?CStr = null,
         job_cond: ?*db.Job.Filter = null,
-        purge_event: u32,
-        purge_job: u32,
-        purge_resv: u32,
-        purge_step: u32,
-        purge_suspend: u32,
-        purge_txn: u32,
-        purge_usage: u32,
+        purge_event: PurgeFlags,
+        purge_job: PurgeFlags,
+        purge_resv: PurgeFlags,
+        purge_step: PurgeFlags,
+        purge_suspend: PurgeFlags,
+        purge_txn: PurgeFlags,
+        purge_usage: PurgeFlags,
+    };
+
+    pub const PurgeAfter = enum(u19) {
+        none = 0,
+        hours = 1 << 16,
+        days = 1 << 17,
+        months = 1 << 18,
+    };
+
+    pub const PurgeFlags = packed struct(u32) {
+        time: PurgeAfter = .none,
+        archive: bool = false,
+        _pad: u12 = 0,
     };
 };
