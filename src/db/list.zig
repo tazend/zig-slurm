308e3adfa714c10930d8f636f20f7b8b8482593e
Toni Harzendorf <toni.harzendorf@gmail.com>
2026-03-26 15:25:33 +0100

    db.List: Add documentation and proper tests
    And renamed / updated some functions

--- src/db/list.zig
+++ src/db/list.zig
@@ -4,19 +4,24 @@ const CStr = common.CStr;
 const xfree_ptr = @import("../SlurmAllocator.zig").slurm_xfree_ptr;
 const NoValue = common.NoValue;
 const Infinite = common.Infinite;
-const db = @import("../db.zig");
+const slurm = @import("../root.zig");
+const db = slurm.db;
 
 pub fn List(comptime T: type) type {
     return opaque {
         const Self = @This();
 
-        const DestroyFunctionSignature = ?*const fn (object: ?*anyopaque) callconv(.C) void;
+        const DestroyFunctionSignature = ?*const fn (object: ?T) callconv(.c) void;
+
+        pub fn noop(object: ?T) callconv(.c) void {
+            _ = object;
+        }
 
         pub extern fn slurmdb_destroy_assoc_usage(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_bf_usage(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_bf_usage_members(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_qos_usage(object: ?*anyopaque) void;
-        pub extern fn slurmdb_destroy_user_rec(object: ?*anyopaque) void;
+        pub extern fn slurmdb_destroy_user_rec(object: ?*db.User) void;
         pub extern fn slurmdb_destroy_account_rec(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_coord_rec(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_clus_res_rec(object: ?*anyopaque) void;
@@ -69,6 +74,18 @@ pub fn List(comptime T: type) type {
         pub extern fn slurmdb_destroy_stats_rec(object: ?*anyopaque) void;
         pub extern fn slurmdb_destroy_slurmdb_stats(stats: *db.Step.Stats) void;
 
+        extern fn slurm_list_append(l: ?*List(T), x: ?T) void;
+        extern fn slurm_list_is_empty(l: ?*List(T)) c_int;
+        extern fn slurm_list_pop(l: ?*List(T)) ?T;
+        extern fn slurm_list_count(l: ?*List(T)) c_int;
+        extern fn slurm_list_destroy(l: ?*List(T)) void;
+        extern fn slurm_list_create(f: DestroyFunctionSignature) ?*List(T);
+        extern fn slurm_list_next(i: ?*Iterator) ?T;
+        extern fn slurm_list_peek_next(i: ?*Iterator) ?T;
+        extern fn slurm_list_iterator_create(l: ?*List(T)) ?*Iterator;
+        extern fn slurm_list_iterator_destroy(i: ?*Iterator) void;
+        extern fn slurm_list_iterator_reset(i: ?*Iterator) void;
+
         const DestroyFunction: DestroyFunctionSignature = switch (T) {
             *db.User => slurmdb_destroy_user_rec,
             *db.Association => slurmdb_destroy_assoc_rec,
@@ -76,81 +93,107 @@ pub fn List(comptime T: type) type {
             else => @compileError("List destruction not implemented for: " ++ @typeName(T)),
         };
 
-        pub const Iterator = struct {
-            const list_itr_t = opaque {};
-            c_handle: *list_itr_t,
-            index: usize = 0,
+        /// The Iterator Type for a List. Whenever you need to traverse the
+        /// List, you need one of them. A List can have multiple Iterators
+        /// active.
+        pub const Iterator = opaque {
 
-            pub extern fn slurm_list_next(i: ?*list_itr_t) ?T;
+            /// Get the next item in the List. If there is no next item, null
+            /// is returned.
             pub fn next(it: *Iterator) ?T {
-                defer it.index += 1;
-                const item = slurm_list_next(it.c_handle);
-                return item;
+                return slurm_list_next(it);
             }
 
-            extern fn slurm_list_iterator_create(l: ?*List(T)) ?*list_itr_t;
-            pub fn init(list: *Self) Iterator {
-                const handle = slurm_list_iterator_create(list);
-                return Iterator{
-                    .c_handle = handle.?,
-                };
+            // list_peek_next is not available in libslurm.so, only libslurmfull.so
+            ///// Get the next item in the List, but without advancing the
+            ///// Iterator. If there is no next item, null is returned.
+            //pub fn peek(it: *Iterator) ?T {
+            //    return slurm_list_peek_next(it);
+            //}
+
+            /// Get an Iterator for the List passed in.
+            pub fn init(list: *Self) *Iterator {
+                return slurm_list_iterator_create(list).?;
             }
 
-            extern fn slurm_list_iterator_destroy(i: ?*list_itr_t) void;
+            /// Deallocate the Iterator.
             pub fn deinit(it: *Iterator) void {
-                slurm_list_iterator_destroy(it.c_handle);
-                it.c_handle = undefined;
+                slurm_list_iterator_destroy(it);
             }
 
-            extern fn slurm_list_iterator_reset(i: ?*list_itr_t) void;
+            /// Reset the Iterator. `next` and `peek` will therefore start from
+            /// the head of the List again.
             pub fn reset(it: *Iterator) void {
-                slurm_list_iterator_reset(it.c_handle);
-                it.index = 0;
+                slurm_list_iterator_reset(it);
             }
         };
 
-        extern fn slurm_list_create(f: DestroyFunctionSignature) ?*List(T);
+        /// Get an `Iterator` for this List.
+        pub fn iter(self: *Self) *Iterator {
+            return .init(self);
+        }
+
+        /// Initialize a new List. The appropriate `DestroyFunction` for `T` is
+        /// automatically detected. This works only for certain Slurm types.
+        /// Use `initNoDestroyItems` or `initWithDestroyFunc` otherwise.
         pub fn init() *List(T) {
             const list = slurm_list_create(DestroyFunction);
-            const list_typed = @as(*List(T), @ptrCast(list.?));
-            return list_typed;
+            //const list_typed = @as(*List(T), @ptrCast(list.?));
+            return list.?;
         }
 
+        /// Initialize a new List with a custom `DestroyFunction`
+        ///
+        /// Note: The function you pass in must be `callconv(.c)`, as it is
+        /// called from within libslurm itself.
         pub fn initWithDestroyFunc(func: DestroyFunctionSignature) *List(T) {
             const list = slurm_list_create(func);
-            const list_typed = @as(*List(T), @ptrCast(list.?));
-            return list_typed;
+            //const list_typed = @as(*List(T), @ptrCast(list.?));
+            return list.?;
         }
 
-        extern fn slurm_list_destroy(l: ?*List(T)) void;
+        /// Initialize a new List, where its items are not automatically freed
+        /// upon running `deinit`.
+        pub fn initNoDestroyItems() *List(T) {
+            return initWithDestroyFunc(noop);
+        }
+
+        /// Destroy this List. Usage of any methods after this function has ran
+        /// will likely result in a program crash.
+        /// If the List was initialised with a `DestroyFunction`, all items in
+        /// this list will automatically get deallocated. This is mostly useful
+        /// when you received a List from the slurmdbd.
+        ///
+        /// If the List was initialized with `initNoDestroyItems`, then only
+        /// the List itself is deallocated, but not its items, so you can
+        /// handle it yourselves, if applicable.
         pub fn deinit(self: *Self) void {
             slurm_list_destroy(self);
         }
 
-        extern fn slurm_list_count(l: ?*List(T)) c_int;
+        /// Get the size of the list
         pub fn size(self: *Self) c_int {
             return slurm_list_count(self);
         }
 
-        pub fn iter(self: *Self) Iterator {
-            return Iterator.init(self);
-        }
-
-        extern fn slurm_list_pop(l: ?*List(T)) ?T;
+        /// Removes an item from the list. Can be used in a `while()` loop if
+        /// you want to iterate all items while also removing them.
         pub fn pop(self: *Self) ?T {
             return slurm_list_pop(self);
         }
 
-        extern fn slurm_list_is_empty(l: ?*List(T)) c_int;
+        /// Checks whether the List is empty.
         pub fn isEmpty(self: *Self) bool {
             return slurm_list_is_empty(self) == 1;
         }
 
-        pub extern fn slurm_list_append(l: ?*List(T), x: ?T) void;
+        /// Append an item to the list.
         pub fn append(self: *Self, item: T) void {
             slurm_list_append(self, item);
         }
 
+        /// All data will be removed from the List, and returned as an allocated slice.
+        /// Caller is responsible for freeing the slice, and all individual items.
         pub fn toOwnedSlice(
             self: *Self,
             allocator: std.mem.Allocator,
@@ -165,11 +208,13 @@ pub fn List(comptime T: type) type {
             }
 
             std.debug.assert(self.size() == 0);
-            self.deinit();
             return data;
         }
 
-        pub fn constSlice(self: *Self, allocator: std.mem.Allocator) std.mem.Allocator.Error![]const T {
+        /// Returns an allocated slice of the data in this List. Caller must
+        /// free the slice returned, but does not own memory of the individual
+        /// items.
+        pub fn toSlice(self: *Self, allocator: std.mem.Allocator) std.mem.Allocator.Error![]T {
             var data: []T = try allocator.alloc(T, @intCast(self.size()));
             var index: usize = 0;
             var list_iter = self.iter();
@@ -182,18 +227,36 @@ pub fn List(comptime T: type) type {
             return data;
         }
 
+        /// Converts the list directly into a `ArrayList(T)`. Caller is
+        /// responsible to free individual items.
         pub fn toArrayList(self: *Self, allocator: std.mem.Allocator) !std.ArrayList(T) {
             return std.ArrayList(T).fromOwnedSlice(
-                allocator,
                 try self.toOwnedSlice(allocator),
             );
         }
 
-        pub fn fromOwnedSlice(items: []T) *List(T) {
-            var list = List(T).init();
-            for (items) |*i| {
-                list.append(@ptrCast(i));
+        /// Append the slice of items to the list.
+        pub fn appendSlice(self: *Self, items: []const T) void {
+            for (items) |i| {
+                self.append(i);
             }
+        }
+
+        /// List takes ownership of the slice. All items will be freed with the
+        /// DestroyFunction with which the List was created.
+        pub fn fromOwnedSlice(items: []T) *List(T) {
+            var list: *List(T) = .init();
+            list.appendSlice(items);
+            return list;
+        }
+
+        /// Takes a slice and uses it, but when `deinit()` is called, the
+        /// individual items are not freed.
+        /// This works nicely if you just want to create a Slurm compatible
+        /// List from an existing ArrayList.
+        pub fn fromSliceUnmanaged(items: []T) *List(T) {
+            var list: *List(T) = .initNoDestroyItems();
+            list.appendSlice(items);
             return list;
         }
     };
@@ -207,4 +270,143 @@ pub fn fromCStr(items: []const [:0]const u8) *List(CStr) {
     return list;
 }
 
-test "List" {}
+test "core" {
+    var list: *List(CStr) = .initNoDestroyItems();
+    defer list.deinit();
+    list.append("item 1");
+    list.append("item 2");
+
+    try std.testing.expectEqual(list.size(), 2);
+    try std.testing.expect(!list.isEmpty());
+
+    {
+        const item = list.pop();
+        const val = slurm.parseCStr(item).?;
+        try std.testing.expectEqualStrings(val, "item 1");
+        try std.testing.expectEqual(list.size(), 1);
+    }
+
+}
+
+test "fromSliceUnmanaged" {
+    const allocator = std.testing.allocator;
+    var list: std.ArrayList(*db.User) = .empty;
+    defer list.deinit(allocator);
+
+    var user: db.User = .{ .name = "user1" };
+    var user2: db.User = .{ .name = "user2" };
+    try list.append(allocator, &user);
+    try list.append(allocator, &user2);
+
+    var slurm_list: *List(*db.User) = .fromSliceUnmanaged(list.items);
+    try std.testing.expectEqual(slurm_list.size(), 2);
+    // This will not free the individual items, since we created the list from
+    // a slice that is externally managed.
+    defer slurm_list.deinit();
+}
+
+test "fromOwnedSlice" {
+    const allocator = slurm.slurm_allocator;
+    var list: std.ArrayList(*db.User) = .empty;
+    defer list.deinit(allocator);
+
+    const user: *db.User = try allocator.create(db.User);
+    user.* = .{ .name = try allocator.dupeZ(u8, "user1") };
+    try list.append(allocator, user);
+
+    var slurm_list: *List(*db.User) = .fromOwnedSlice(try list.toOwnedSlice(allocator));
+    try std.testing.expectEqual(slurm_list.size(), 1);
+    defer slurm_list.deinit();
+}
+
+test "Iterator" {
+    var list: *List(*db.User) = .initNoDestroyItems();
+    defer list.deinit();
+
+    var user: db.User = .{ .name = "user1" };
+    var user2: db.User = .{ .name = "user2" };
+    list.append(&user);
+    list.append(&user2);
+
+    var iter = list.iter();
+    defer iter.deinit();
+
+    var index: isize = 0;
+    while (iter.next()) |item| {
+        const name = slurm.parseCStr(item.name).?;
+        try std.testing.expectStringStartsWith(name, "user");
+        index += 1;
+    }
+
+    try std.testing.expectEqual(list.size(), index);
+
+    iter.reset();
+    {
+        const item = iter.next().?;
+        const name = slurm.parseCStr(item.name).?;
+        try std.testing.expectEqualStrings(name, "user1");
+        try std.testing.expect(iter.next() != null);
+    }
+
+    //  {
+    //      const item = iter.peek().?;
+    //      const name = slurm.parseCStr(item.name).?;
+    //      try std.testing.expectEqualStrings(name, "user2");
+    //  }
+}
+
+test "toSlice, toOwnedSlice and toArrayList" {
+    const allocator = std.testing.allocator;
+    {
+        var list: *List(CStr) = .initNoDestroyItems();
+        defer list.deinit();
+        list.append("item 1");
+
+        const slice = try list.toSlice(allocator);
+        defer allocator.free(slice);
+
+        const item = std.mem.span(slice[0]);
+        try std.testing.expectEqualStrings("item 1", item);
+    }
+    {
+        var list: *List(CStr) = .initNoDestroyItems();
+        defer list.deinit();
+        list.append("item 1");
+
+        const ownedSlice = try list.toOwnedSlice(allocator);
+        defer allocator.free(ownedSlice);
+
+        const item = std.mem.span(ownedSlice[0]);
+        try std.testing.expectEqualStrings("item 1", item);
+    }
+    {
+        var list: *List(CStr) = .initNoDestroyItems();
+        defer list.deinit();
+        list.append("item 1");
+
+        var array_list = try list.toArrayList(allocator);
+        defer array_list.deinit(allocator);
+
+        const item = std.mem.span(array_list.items[0]);
+        try std.testing.expectEqualStrings("item 1", item);
+    }
+}
+
+test "appendSlice" {
+    {
+        var list: *List(*db.User) = .initNoDestroyItems();
+        defer list.deinit();
+
+        var user: db.User = .{ .name = "user1" };
+        var user2: db.User = .{ .name = "user2" };
+
+        list.appendSlice(&.{ &user, &user2 });
+    }
+    {
+        var list: *List(CStr) = .initNoDestroyItems();
+        defer list.deinit();
+
+        list.appendSlice(&.{ "item1", "item2" });
+    }
+}
+
