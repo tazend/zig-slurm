bd831c10c11034b00e6c025c4ebd97efc8147edb
Toni Harzendorf <toni.harzendorf@gmail.com>
2026-06-19 14:46:01 +0200

    add more flags/enums

--- src/slurm-ext.zig
+++ src/slurm-ext.zig
@@ -27,6 +27,234 @@ pub const ShowFlags = packed struct(u16) {
     pub const full: ShowFlags = .{ .all = true, .detail = true };
 };
 
+pub const SelectType = packed struct(u16) {
+    cpu: bool = false,
+    socket: bool = false,
+    core: bool = false,
+    board: bool = false,
+    memory: bool = false,
+    linear: bool = false,
+    enforce_binding_gres: bool = false,
+    one_task_per_sharing_gres: bool = false,
+    one_task_per_core: bool = false,
+    pack_nodes: bool = false,
+    ll_shared_gres: bool = false,
+    _p1: u1 = 0,
+    core_default_dist_block: bool = false,
+    _p2: u1 = 0,
+    lln: bool = false,
+    multiple_sharing_gres_pj: bool = false,
+};
+
+pub const CPUBinding = packed struct(u16) {
+    verbose: bool = false,
+    @"type": BindType,
+    one_thread_per_core: bool = false,
+    _p1: u3 = 0,
+
+    pub const BindType = enum(u12) {
+        threads = 2,
+        cores = 4,
+        sockets = 8,
+        ldoms = 16,
+        none = 0x0020,
+        map = 0x0080,
+        mask = 0x0100,
+        ldrank = 0x0200,
+        ldmap = 0x0400,
+        ldmask = 0x0800,
+    };
+};
+
+pub const TaskPluginParams = packed struct(u32) {
+    auto_bind: AutoBinding,
+    slurmd_off_spec: bool = false, // 1 << 18
+    cpu_bind_off: bool = false,
+    oom_kill_spec: bool = false,
+    slurmd_spec_override: bool = false,
+    _p1: u11 = 0,
+
+    pub const AutoBinding = enum(u18) {
+        threads = 0x04000,
+        cores = 0x10000,
+        sockets = 0x20000,
+    };
+};
+
+pub const MemoryBinding = packed struct(u16) {
+    verbose: bool = false,
+    @"type": BindType,
+    sort: bool = false,
+    prefer: bool = false, // 1 << 7
+    _p1: u9 = 0,
+
+    pub const BindType = enum(u6) {
+        none,
+        rank,
+        map,
+        mask,
+        local,
+    };
+};
+
+pub const PriorityFlags = packed struct(u16) {
+    accrue_always: bool = false,
+    max_tres: bool = false,
+    size_relative: bool = false,
+    depth_oblivious: bool = false,
+    calculate_running: bool = false,
+    fair_tree: bool = false,
+    increase_only: bool = false,
+    no_normal: NoNormal,
+    max_tres_gres: bool = false,
+    _p1: u5 = false,
+
+
+    pub const NoNormal = packed struct(u4) {
+        assoc: bool = false,
+        part: bool = false,
+        qos: bool = false,
+        tres: bool = false,
+
+        pub const all: NoNormal = .{ .assoc = true, .part = true, .qos = true, .tres = true, };
+    };
+};
+
+pub const PrivateData = packed struct(u16) {
+    jobs: bool = false,
+    nodes: bool = false,
+    partitions: bool = false,
+    usage: bool = false,
+    users: bool = false,
+    accounts: bool = false,
+    reservations: bool = false,
+    _p1: u1 = 0,
+    events: bool = false,
+    _p2: u7 = 0,
+};
+
+pub const PriorityResetPeriod = enum(u16) {
+    none = 0,
+    now,
+    daily,
+    weekly,
+    monthly,
+    quarterly,
+    yearly,
+};
+
+pub const JobFlags = packed struct(u64) {
+    kill_on_invalid_dependency: bool = false,
+    no_kill_on_invalid_dependency: bool = false,
+    has_state_dir: bool = false,
+    backfill_test_in_progress: bool = false,
+    enforce_gres_bind: bool = false,
+    test_now_only: bool = false,
+    send_environment_to_db: bool = false,
+    grace_preempt: bool = false,
+    spread: bool = false,
+    prefer_min_nodes: bool = false,
+    kill_hurry: bool = false,
+    sib_job_flush: bool = false,
+    is_heterogenous: bool = false,
+    ntasks_set: bool = false,
+    cpus_set: bool = false,
+    backfill_whole_node_test: bool = false,
+    top_prio_tmp: bool = false,
+    accrue_over: bool = false,
+    disable_gres_bind: bool = false,
+    was_running: bool = false,
+    reset_accrue_time: bool = false,
+    is_cron_job: bool = false,
+    memory_set: bool = false,
+    external: bool = false,
+    uses_default_account: bool = false,
+    uses_default_partition: bool = false,
+    uses_default_qos: bool = false,
+    dependent: bool = false,
+    magnetic_reservation: bool = false,
+    partition_assigned: bool = false,
+    backfill_sched: bool = false,
+    backfill_last: bool = false,
+    tasks_changed: bool = false,
+    send_script_to_dbd: bool = false,
+    reset_licenses_per_task: bool = false,
+    reset_licenses_per_job: bool = false,
+    gres_one_task_per_sharing: bool = false,
+    gres_multiple_tasks_per_sharing: bool = false,
+    gres_allow_task_sharing: bool = false,
+    stepmgr_enabled: bool = false,
+    purge_heterogenous_job: bool = false,
+    spread_segments: bool = false,
+    consolidate_segments: bool = false,
+    expedited_requeue: bool = false,
+};
+
+pub const X11ForwardNode = packed struct(u16) {
+    all: bool = false,
+    batch: bool = false,
+    first: bool = false,
+    last: bool = false,
+};
+
+pub const ReservationFlags = packed struct(u64) {
+    maintenace: bool = false,
+    no_maintenance: bool = false,
+    daily: bool = false,
+    no_daily: bool = false,
+    weekly: bool = false,
+    no_weekly: bool = false,
+    ignore_running_jobs: bool = false,
+    no_ignore_running_jobs: bool = false,
+    any_nodes: bool = false,
+    no_any_nodes: bool = false,
+    static: bool = false,
+    no_static: bool = false,
+    partition_nodes: bool = false,
+    no_partition_nodes: bool = false,
+    overlap: bool = false,
+    specific_nodes: bool = false,
+    time_float: bool = false,
+    replace: bool = false,
+    all_nodes: bool = false,
+    purge_after_last_job_done: bool = false,
+    weekday: bool = false,
+    no_weekday: bool = false,
+    weekend: bool = false,
+    no_weekend: bool = false,
+    flex: bool = false,
+    no_flex: bool = false,
+    add_duration: bool = false,
+    remove_duration: bool = false,
+    no_hold_jobs: bool = false,
+    replace_down_nodes: bool = false,
+    no_purge_after_last_job_done: bool = false,
+    magnetic: bool = false,
+    no_magnetic: bool = false,
+    skip: bool = false,
+    hourly: bool = false,
+    no_hourly: bool = false,
+    gres_required: bool = false,
+    allow_user_deletion: bool = false,
+    no_allow_user_deletion: bool = false,
+    sched_failed: bool = false,
+    force_start: bool = false,
+};
+
+pub const PreemptMode = packed struct(u16) {
+    @"suspend": bool = false,
+    requeue: bool = false,
+    _p1: u1 = 0,
+    cancel: bool = false,
+    off: bool = false,
+    priority: bool = false,
+    within: bool = false,
+    gang: bool = false,
+    _p2: u8 = 0,
+
+    pub const none: PreemptMode = @bitCast(@as(u16, 0));
+};
+
 pub const AccountingGatherEnergy = extern struct {
     ave_watts: u32,
     base_consumed_energy: u64,
