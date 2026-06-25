f3a0967afa7c43a435b4d9cbbfa2d03d150d22d7
Toni Harzendorf <toni.harzendorf@gmail.com>
2026-06-25 15:15:14 +0200

    db.event: add type and filter flags

--- src/db/event.zig
+++ src/db/event.zig
@@ -9,7 +9,7 @@ const Connection = db.Connection;
 pub const Event = extern struct {
     cluster: ?CStr = null,
     cluster_nodes: ?CStr = null,
-    event_type: u16,
+    event_type: db.Event.Type,
     node_name: ?CStr = null,
     period_end: time_t = 0,
     period_start: time_t = 0,
@@ -18,12 +18,18 @@ pub const Event = extern struct {
     state: u32,
     tres_str: ?CStr = null,
 
+    pub const Type = enum(u16) {
+        all = 0,
+        cluster,
+        node,
+    };
+
     pub const Filter = extern struct {
         cluster_list: ?*List(*opaque {}) = null,
-        cond_flags: u32,
+        cond_flags: Filter.Flags,
         cpus_max: u32,
         cpus_min: u32,
-        event_type: u16,
+        event_type: db.Event.Type,
         format_list: ?*List(*opaque {}) = null,
         node_list: ?CStr = null,
         period_end: time_t = 0,
@@ -31,5 +37,9 @@ pub const Event = extern struct {
         reason_list: ?*List(*opaque {}) = null,
         reason_uid_list: ?*List(*opaque {}) = null,
         state_list: ?*List(*opaque {}) = null,
+
+        pub const Flags = packed struct(u32) {
+            only_open: bool = false,
+        };
     };
 };
